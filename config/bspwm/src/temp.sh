db8220f0f1fa84ef0a3c840061dd2aa584121599
CristianmMst <cristianmmst@gmail.com>
2024-09-06 04:32:49 +0000

    support temp to amd

--- config/bspwm/src/temp.sh
+++ config/bspwm/src/temp.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-TEMP="$(cat /sys/devices/platform/coretemp.*/hwmon/hwmon*/temp2_input | awk '{sub(/000$/, "°C", $0); print}')"
+TEMP="$(sensors | grep 'Tctl' | awk '{print $2}')"
+# TEMP="$(cat /sys/devices/platform/coretemp.*/hwmon/hwmon*/temp2_input | awk '{sub(/000$/, "°C", $0); print}')"
 echo "${TEMP:-null}"
